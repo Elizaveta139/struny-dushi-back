1632e7f1022724c3f06f5f087f53d55b81ae3a8f
Elizaveta139 <elizavetarevina139@gmail.com>
2024-08-20 09:27:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ dotenv.config();
 //////////////////////////////////
 // шаг 1
 
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const REDIRECT_URI = process.env.REDIRECT_URI;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 // const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
